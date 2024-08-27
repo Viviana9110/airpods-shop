c7e2c7deb75a352337313420dfe79c9988c810a4
unknown <v.londono.naranjo@gmail.com>
2024-08-27 15:04:27 -0500

    Tienda virtual para airpods

---
+++ airpods/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Hero from "./components/Header";
+
+const App = () => {
+  return <main className="overflow-x-hidden">
+    <Hero/>
+  </main>
+};
+
+export default App;
---
+++ airpods/src/data/MockData.js
@@ -0,0 +1,59 @@
+import Img1 from "../assets/1.png";
+import Img2 from "../assets/2.png";
+import Img3 from "../assets/3.png";
+
+export const NavbarData = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "Categories",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    link: "#",
+  },
+];
+
+export const HeadphoneData = [
+  {
+    id: 1,
+    Image: Img1,
+    title: "Apple Airpods Wireless Over-Ear Headphones",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet metus sit amet libero malesuada molestie a at sem. Suspendisse dictum lacus ante, non auctor justo tristique ac. Integer consequat leo sed odio posuere ultrices.",
+    price: "$49.99",
+    modal: "AirPods",
+    bgColor: "#e85950",
+  },
+  {
+    id: 2,
+    Image: Img2,
+    title: "Apple Airpods Max Wireless Over-Ear Headphones",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet metus sit amet libero malesuada molestie a at sem. Suspendisse dictum lacus ante, non auctor justo tristique ac. Integer consequat leo sed odio posuere ultrices.",
+    price: "$100.00",
+    modal: "Max",
+    bgColor: "#24b4bc",
+  },
+  {
+    id: 3,
+    Image: Img3,
+    title: "Apple Airpods Pro Wireless Over-Ear Headphones",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet metus sit amet libero malesuada molestie a at sem. Suspendisse dictum lacus ante, non auctor justo tristique ac. Integer consequat leo sed odio posuere ultrices.",
+    price: "$100.00",
+    modal: "Pro",
+    bgColor: "#7aa10e",
+  },
+];
